a695cb3aec261bc2a7dff25aa195248bc1ffd7e6
marcel <mocmocsm@gmail.com>
2024-05-30 18:25:58 +0200

    mod v 1.0

--- htdocs/wp-content/plugins/dokan-lite/templates/store.php
+++ htdocs/wp-content/plugins/dokan-lite/templates/store.php
@@ -1,36 +1,92 @@
 <?php
 /**
- * The Template for displaying vendor store.
+ * The Template for displaying all single posts.
  *
  * @package dokan
+ * @package dokan - 2014 1.0
  */
 
-get_header();
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
 
-$store_user = dokan()->vendor->get( get_query_var( 'author' ) );
-$store_info = $store_user->get_shop_info();
-$social_info = $store_user->get_social_profiles();
-$store_tabs = dokan_get_store_tabs( $store_user->get_id() );
-$store_address = dokan_get_seller_short_address( $store_user->get_id(), false );
+$store_user   = dokan()->vendor->get( get_query_var( 'author' ) );
+$store_info   = $store_user->get_shop_info();
 $map_location = $store_user->get_location();
-?>
 
-<div class="dokan-store-container">
+get_header( 'shop' );
+
+if ( function_exists( 'yoast_breadcrumb' ) ) {
+    yoast_breadcrumb( '<p id="breadcrumbs">', '</p>' );
+}
+?>
+<?php do_action( 'woocommerce_before_main_content' ); ?>
+
+<div class="dokan-store-wrap layout-<?php echo esc_attr( $layout ); ?>" style="max-width: 80%; margin: 0 auto;">
+
+    <?php if ( 'left' === $layout ) { ?>
+        <?php
+        dokan_get_template_part(
+            'store', 'sidebar', [
+                'store_user'   => $store_user,
+                'store_info'   => $store_info,
+                'map_location' => $map_location,
+            ]
+        );
+        ?>
+    <?php } ?>
+
     <div id="dokan-primary" class="dokan-single-store">
         <div id="dokan-content" class="store-page-wrap woocommerce" role="main">
-            <div class="store-header-wrap">
-                <?php dokan_get_template_part( 'store-header' ); ?>
-            </div>
 
-            <div class="store-content-wrap">
-                <div id="dokan-content-sidebar" class="dokan-content-sidebar col-md-3 col-sm-12 col-xs-12">
-                    <?php do_action( 'dokan_sidebar_store_before', $store_user->data, $store_info ); ?>
-                    <div class="dokan-store-widget-wrap">
-                        <?php do_action( 'dokan_store_profile_frame_after', $store_user->data, $store_info ); ?>
-                    </div>
-                    <?php do_action( 'dokan_sidebar_store_after', $store_user->data, $store_info ); ?>
+            <?php dokan_get_template_part( 'store-header' ); ?>
+
+            <?php do_action( 'dokan_store_profile_frame_after', $store_user->data, $store_info ); ?>
+
+            <?php if ( have_posts() ) { ?>
+
+                <div class="seller-items">
+
+                    <?php woocommerce_product_loop_start(); ?>
+
+                    <?php
+                    while ( have_posts() ) :
+                        the_post();
+                        ?>
+
+                        <?php wc_get_template_part( 'content', 'product' ); ?>
+
+                    <?php endwhile; // end of the loop. ?>
+
+                    <?php woocommerce_product_loop_end(); ?>
+
                 </div>
 
-                <div id="dokan-primary" class="dokan-single-store col-md-9 col-sm-12 col-xs-12">
-                    <?php if ( $store_tabs ) : ?>
-            
+                <?php dokan_content_nav( 'nav-below' ); ?>
+
+            <?php } else { ?>
+
+                <p class="dokan-info"><?php esc_html_e( 'No products were found of this vendor!', 'dokan-lite' ); ?></p>
+
+            <?php } ?>
+        </div>
+
+    </div><!-- .dokan-single-store -->
+
+    <?php if ( 'right' === $layout ) { ?>
+        <?php
+        dokan_get_template_part(
+            'store', 'sidebar', [
+                'store_user'   => $store_user,
+                'store_info'   => $store_info,
+                'map_location' => $map_location,
+            ]
+        );
+        ?>
+    <?php } ?>
+
+</div><!-- .dokan-store-wrap -->
+
+<?php do_action( 'woocommerce_after_main_content' ); ?>
+
+<?php get_footer( 'shop' ); ?>
